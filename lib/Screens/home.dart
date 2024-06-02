584426342839c728e7f928bb04231290ce507857
choudharyuzair2126 <uzair2126@proton.me>
2024-06-02 14:46:06 +0500

    Initial commit

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:recipie_app/Screens/favourite.dart';
 import 'package:recipie_app/Screens/recipiescreen.dart';
 import 'package:recipie_app/Services/injection.dart';
+import 'package:recipie_app/Services/uihelper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:recipie_app/Services/recipiemodel.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -17,6 +22,7 @@ class _MyHomePageState extends State<MyHomePage> {
     getRecipies();
   }
 
+  //List<String> favorited = ["", ""];
   bool isloading = true;
   RecipieApi recipies = RecipieApi();
   getRecipies() {
@@ -33,8 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.blue.shade600,
+    return SafeArea(
+      bottom: false,
+      maintainBottomViewPadding: false,
+      child: Scaffold(
+        backgroundColor: Colors.blue.shade100,
         appBar: AppBar(
           title: const Text("Recipies App"),
           backgroundColor: Colors.blue,
@@ -108,11 +117,59 @@ class _MyHomePageState extends State<MyHomePage> {
                                     ),
                                   ],
                                 ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 14.0, vertical: 5),
+                                child: SizedBox(
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        final prefs = await SharedPreferences
+                                            .getInstance();
+                                        List<String> favorited =
+                                            prefs.getStringList('favorited') ??
+                                                [];
+
+                                        if (favorited
+                                            .contains(index.toString())) {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            const SnackBar(
+                                                duration:
+                                                    Duration(milliseconds: 500),
+                                                content: Text(
+                                                    "Already added to favorites")),
+                                          );
+                                        } else {
+                                          favorited.add(index.toString());
+                                          await prefs.setStringList(
+                                              'favorited', favorited);
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            const SnackBar(
+                                                duration:
+                                                    Duration(milliseconds: 500),
+                                                content:
+                                                    Text("Added to favorites")),
+                                          );
+                                        }
+                                      },
+                                      child: const Text("Add to favorites"),
+                                    )),
                               )
                             ],
                           ),
                         ),
                       );
-                    })));
+                    })),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            UiHelper.push1(context, const Faourite());
+          },
+          child: const Icon(Icons.favorite),
+        ),
+      ),
+    );
   }
 }
